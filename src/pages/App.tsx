4698e6d9b35265bdf6ee2ff9cafc8ff33c442f91
SamuelV1 <s4muelvic@outlook.com>
2022-02-04 18:52:35 -0300

    local Wallet

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,44 +1,51 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import localforage from 'localforage'
 
 interface collection {
-    WalletName: string,
-    coins: object,
+  WalletName: string,
+  coins: object,
+}
+const Initialfiles: collection =
+{
+    WalletName: "placeholder",
+    coins: {},
+    
 }
 
 function App() {
-const [localWallet, setlocal] = useState('')
+  const [localWallet, setlocal] = useState<collection[]>()
 
-async function createWallet(){
-console.log('Carteira criada')
-let data = [{
-	WalletName: "mining233",
-	coins: {
-	bitcoin: "preço",
-	etherun: "preço"
-	}
-}]
-const localwallet = await localforage.getItem<collection[]>('stashWallet')
-
-	if(localwallet){
-	localwallet.concat(data)
- 	localforage.setItem('stashWallet', localwallet)
-	} else localforage.setItem('stashWallet', data)
+  async function createWallet() {
+   
+    let data = [{
+      WalletName: "mining233",
+      coins: {
+        bitcoin: "preço",
+        etherun: "preço"
+      }
+    }]
 
 
-}
- useEffect(() => {
-        async function start(){
-            const StoreData = await localforage.getItem('stashWallet')
-          if (StoreData){
-          console.log(StoreData)
-          }
+    if (localWallet) {
+      localWallet.concat(data)
+      localforage.setItem('stashWallet', localWallet)
+    } else console.log("não sei bixo")
 
-        }
 
-        start()
+  }
+  useEffect(() => {
+    async function start() {
+      const StoreData = await localforage.getItem<collection[]>('stashWallet')
+      if (StoreData) {
+        console.log("informações guardadas: ",StoreData)
+        setlocal(StoreData)
+      }
 
-      }, [])
+    }
+
+    start()
+
+  }, [])
 
   return (
     <div className="App">
